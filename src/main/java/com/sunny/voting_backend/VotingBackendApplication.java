9a7c0d8a50849ce2f7a21e9bb7e6182fc8339677
MemoCode101 <sunnythakur8691807411@gmail.com>
2026-01-05 15:19:24 +0530

    Initial Setup: Project Structure and Database Configuration

---
+++ src/main/java/com/sunny/voting_backend/VotingBackendApplication.java
@@ -0,0 +1,17 @@
+package com.sunny.voting_backend;
+
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+
+public class VotingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VotingBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sunny/voting_backend/VotingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunny.voting_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
